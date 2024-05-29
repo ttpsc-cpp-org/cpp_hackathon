a3b4bbd7dd5ddc1a881d94e0ef24ff6b8aa43c74
Przemysław Sokołowski <przemyslaw.sokolowski@ttpsc.com>
2024-05-29 14:15:15 +0200

    Added gcov

--- PWTests/PWTests.cpp
+++ PWTests/PWTests.cpp
@@ -2,10 +2,10 @@
 #include <PWInventory.h>
 
 // TIP: Keep the tests simple, aim for good coverage 
-TEST(PWInventory, FooBad) {
-	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
-	EXPECT_EQ(inventory[0].name, "Bar");
-}
+// TEST(PWInventory, FooBad) {
+// 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
+// 	EXPECT_EQ(inventory[0].name, "Bar");
+// }
 TEST(PWInventory, FooGood) {
 	PWInventory inventory{ { InventoryItem{ "Foo", 0 , 0 } } };
 	EXPECT_EQ(inventory[0].name, "Foo");
