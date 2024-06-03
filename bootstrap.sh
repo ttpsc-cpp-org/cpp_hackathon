ee9df49b0445d1d3317dfcebe8e38dae914408e5
Maciej Czarnecki <mcczarny@gmail.com>
2024-06-03 18:23:28 +0200

    Add more dependencies in the boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
 #/bin/sh
+set -e
+
+if command -v apt-get >/dev/null 2>&1; then
+    apt-get update
+    apt-get install -y python3 python3-pip cmake ninja-build python3-venv
+fi
+
+python3 -m venv venv
+. venv/bin/activate
+pip3 install gcovr "conan==1.64.1"
 conan config install ./conan
-pip install gcovr
+
+echo "Use 'source venv/bin/activate' to activate the virtual environment."
