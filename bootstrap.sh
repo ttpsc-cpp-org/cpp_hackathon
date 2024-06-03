6026ffe5e8bab69326eaaff6320c4387b43227c9
Maciej Czarnecki <mcczarny@gmail.com>
2024-06-03 18:47:49 +0200

    fixup! Add more dependencies in the boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #/bin/sh
-apt-get update
-apt-get install -y python3 python3-pip cmake ninja-build
+if command -v apt-get >/dev/null 2>&1; then
+    apt-get update
+    apt-get install -y python3 python3-pip cmake ninja-build python3-venv
+fi
+
+python3 -m venv venv
+. venv/bin/activate
 pip install gcovr "conan==1.64.1"
 conan config install ./conan
